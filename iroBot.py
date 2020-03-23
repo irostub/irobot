c42c45178dafd3b155644b91b7b8f1c37559be98
irostub <irostub@gmail.com>
2020-03-24 06:22:58 +0900

    Fix extension load exception message print detail

--- iroBot.py
+++ iroBot.py
@@ -3,22 +3,30 @@ import settings
 from discord.ext import commands
 
 token = settings.token
-client = commands.Bot(command_prefix= '!이로 ',help_command=None)
+client = commands.Bot(command_prefix='!이로 ', help_command=None)
 
-extensions = ['cogs.GameCommands','cogs.HelpCommands','cogs.ReplyCommands','game.chat.ChatGame']
+extensions = ['cogs.GameCommands', 'cogs.HelpCommands', 'cogs.ReplyCommands', 'game.chat.ChatGame']
 
 '''봇이 시작될 때'''
+
+
 @client.event
 async def on_ready():
     print("iro bot is ready")
-    game = discord.Game("!이로 도움말")
-    await client.change_presence(status=discord.Status.online, activity= game)
+    game = discord.Game("!이로 도움")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+# 채널 message에서 해당 채널의 모든 맴버를 get
+# @client.event
+# async def on_message(message):
+#     mem = message.channel.members
+#     print(mem)
 
 for extensions in extensions:
     try:
         client.load_extension(extensions)
     except Exception as error:
-        print("x")
+        print('{} cannot be loaded. [{}]'.format(extensions, error))
 
 '''RUN BOT'''
 client.run(token)
