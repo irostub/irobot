6d273e86c0ed9743dc74ddec387bc2e99d1029a8
irostub <irostub@gmail.com>
2020-03-25 22:19:52 +0900

    Add learn table to implement the Q & A function

--- DATABASE.sql
+++ DATABASE.sql
@@ -16,34 +16,51 @@
 CREATE DATABASE IF NOT EXISTS `irobot` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `irobot`;
 
--- 테이블 irobot.chat_player 구조 내보내기
-CREATE TABLE IF NOT EXISTS `chat_player` (
+-- 테이블 irobot.learn 구조 내보내기
+CREATE TABLE IF NOT EXISTS `learn` (
   `id` varchar(50) NOT NULL DEFAULT '',
-  `exp` int(50) NOT NULL DEFAULT 0,
+  `nick` varchar(50) NOT NULL DEFAULT '',
+  `input` varchar(100) NOT NULL DEFAULT '',
+  `output` varchar(200) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- 테이블 데이터 irobot.learn:~0 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `learn` DISABLE KEYS */;
+INSERT INTO `learn` (`id`, `nick`, `input`, `output`) VALUES
+	('351427090123587587', 'iro_bound', '안녕하세오', '오냐');
+/*!40000 ALTER TABLE `learn` ENABLE KEYS */;
+
+-- 테이블 irobot.user 구조 내보내기
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` varchar(50) NOT NULL DEFAULT '',
+  `exp` int(11) NOT NULL DEFAULT 0,
   `level` int(11) NOT NULL DEFAULT 1,
   `date` varchar(100) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- 테이블 데이터 irobot.chat_player:~14 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `chat_player` DISABLE KEYS */;
-INSERT INTO `chat_player` (`id`, `exp`, `level`, `date`) VALUES
+-- 테이블 데이터 irobot.user:~15 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `exp`, `level`, `date`) VALUES
 	('169678500893163520', 20, 1, '0'),
 	('172002275412279296', 0, 1, '0'),
 	('184405311681986560', 15, 1, '0'),
 	('185476724627210241', 15, 1, '0'),
-	('252025791007817728', 35, 1, '0'),
-	('277080328269594625', 40, 1, '1585119357'),
+	('252025791007817728', 50, 1, '1585137121'),
+	('277080328269594625', 65, 2, '1585141515'),
+	('290116665968951296', 0, 1, '1585134631'),
 	('305630277852594176', 0, 1, '0'),
-	('324196735985647627', 610, 6, '1585119240'),
-	('351427090123587587', 115, 2, '1585119938'),
-	('378562158260256789', 100, 2, '1585110720'),
-	('384064343714693122', 25, 1, '0'),
+	('313361077813641216', 0, 1, '1585130410'),
+	('324196735985647627', 630, 6, '1585138678'),
+	('351427090123587587', 295, 4, '1585141916'),
+	('378562158260256789', 120, 2, '1585138669'),
+	('384064343714693122', 40, 1, '1585142059'),
 	('389267978425860096', 300, 4, '0'),
-	('399828403659997214', 95, 2, '1585119946'),
+	('399828403659997214', 100, 2, '1585137805'),
 	('418995840338755584', 45, 1, '1585111217'),
 	('423843986122342401', 0, 1, '1585125783');
-/*!40000 ALTER TABLE `chat_player` ENABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
