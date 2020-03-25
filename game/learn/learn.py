88abf52262209d8bde473d2df2b1e7856ab88a13
irostub <irostub@gmail.com>
2020-03-25 22:15:40 +0900

    Implements the ability to learn questions and answers
    Need to implement sending a response message after learning.

---
+++ game/learn/learn.py
@@ -0,0 +1,44 @@
+from discord.ext import commands
+import discord
+import pymysql
+
+class Learn(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+        self.conn = pymysql.connect(host = 'localhost', user = 'root', password = '', db = 'irobot', charset = 'utf8')
+        self.curs = self.conn.cursor()
+        self.sql = ""
+
+    @commands.command(name="학습")
+    async def Command_study(self, ctx, *args):
+        input = args[0]
+        output = ""
+        for i in range(len(args)):
+            if i == 0:
+                continue
+            if i == len(args)-1:
+                output = output + args[i]
+            else:
+                output = output + args[i]+" "
+
+        id = ctx.author.id
+        teacher = ctx.author.name
+
+        self.sql = "select * from learn where input = %s limit 1;"
+
+        rows_count = self.curs.execute(self.sql, input)
+        rows = self.curs.fetchall()
+
+        if rows_count > 0:
+            await ctx.channel.send("이미 알고있던거 있음")
+        else:
+            self.sql = "insert into learn values(%s ,%s ,%s ,%s); "
+            self.curs.execute(self.sql, (id, teacher, input, output))
+            self.conn.commit()
+
+
+
+
+def setup(client):
+    client.add_cog(Learn(client))
